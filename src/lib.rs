5d97751aa75df7b06d25df04c42d0a457f529407
skaunov <skaunov@disroot.org>
2023-08-02 11:48:49 +0300

    Improve methods naming
    as suggested at https://users.rust-lang.org/t/traits-workshop/97832/5
    
    `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,24 @@ pub struct Planet {
 }
 
 impl Object for Planet {
-    fn is_gravity_source(&self) -> bool {true}
-    fn is_gravity_receiver(&self) -> bool {false}    
-    fn coordinate(&mut self) -> &mut Coordinate {&mut self.coordinate}
-    fn weight(&self) -> Option<i32> {Some(self.weight)}
-    fn velocity(&mut self) -> Option<&mut Direction> {None}
-    fn get_coordinate(&self) -> Coordinate {self.coordinate}
+    fn is_gravity_source(&self) -> bool {
+        true
+    }
+    fn is_gravity_receiver(&self) -> bool {
+        false
+    }
+    fn coordinate_mut(&mut self) -> &mut Coordinate {
+        &mut self.coordinate
+    }
+    fn weight(&self) -> Option<i32> {
+        Some(self.weight)
+    }
+    fn velocity_mut(&mut self) -> Option<&mut Direction> {
+        None
+    }
+    fn coordinate(&self) -> Coordinate {
+        self.coordinate
+    }
 }
 
 #[derive(Clone)]
@@ -36,34 +48,52 @@ pub struct Asteroid {
 }
 
 impl Object for Asteroid {
-    fn is_gravity_source(&self) -> bool {false}
-    fn is_gravity_receiver(&self) -> bool {true}
-    fn coordinate(&mut self) -> &mut Coordinate {&mut self.coordinate}
-    fn weight(&self) -> Option<i32> {None}
-    fn velocity(&mut self) -> Option<&mut Direction> {Some(&mut self.velocity)}
-    fn get_coordinate(&self) -> Coordinate {self.coordinate}
+    fn is_gravity_source(&self) -> bool {
+        false
+    }
+    fn is_gravity_receiver(&self) -> bool {
+        true
+    }
+    fn coordinate_mut(&mut self) -> &mut Coordinate {
+        &mut self.coordinate
+    }
+    fn weight(&self) -> Option<i32> {
+        None
+    }
+    fn velocity_mut(&mut self) -> Option<&mut Direction> {
+        Some(&mut self.velocity)
+    }
+    fn coordinate(&self) -> Coordinate {
+        self.coordinate
+    }
 }
 
 pub trait Object {
     fn is_gravity_source(&self) -> bool;
     fn is_gravity_receiver(&self) -> bool;
-    fn coordinate(&mut self) -> &mut Coordinate;
-    fn get_coordinate(&self) -> Coordinate;
+    fn coordinate_mut(&mut self) -> &mut Coordinate;
+    fn coordinate(&self) -> Coordinate;
     fn weight(&self) -> Option<i32>;
-    fn velocity(&mut self) -> Option<&mut Direction>;
+    fn velocity_mut(&mut self) -> Option<&mut Direction>;
 }
 
 fn get_distance(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
     (((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) as f64).sqrt() as i32
 }
 
-fn apply_physics(mut objects: Vec<Box<dyn Object>>, gravitational_constant: i32) -> Vec<Box<dyn Object>> {
+fn apply_physics(
+    mut objects: Vec<Box<dyn Object>>,
+    gravitational_constant: i32,
+) -> Vec<Box<dyn Object>> {
     // Go through each pair of objects, and apply
     let gravity_sources = objects
         .iter()
         .filter_map(|o| {
             return if o.is_gravity_source() {
-                Some((o.get_coordinate().clone(), o.weight().expect(BREAKING_VALUE_KIND)))
+                Some((
+                    o.coordinate().clone(),
+                    o.weight().expect(BREAKING_VALUE_KIND),
+                ))
             } else {
                 None
             };
@@ -94,10 +124,10 @@ fn apply_physics(mut objects: Vec<Box<dyn Object>>, gravitational_constant: i32)
                             * gravitational_constant
                             / distance,
                     };
-                    asteroid.velocity().expect(BREAKING_VALUE_KIND).x -= force.x;
-                    asteroid.velocity().expect(BREAKING_VALUE_KIND).y -= force.y;
+                    asteroid.velocity_mut().expect(BREAKING_VALUE_KIND).x -= force.x;
+                    asteroid.velocity_mut().expect(BREAKING_VALUE_KIND).y -= force.y;
 
-                    let vel = asteroid.velocity().expect(BREAKING_VALUE_KIND);
+                    let vel = asteroid.velocity_mut().expect(BREAKING_VALUE_KIND);
                 })
         }
     });
@@ -105,8 +135,8 @@ fn apply_physics(mut objects: Vec<Box<dyn Object>>, gravitational_constant: i32)
     // Apply the new velocity to each object.
     objects.iter_mut().for_each(|object| {
         if object.is_gravity_receiver() {
-            object.coordinate().x += object.velocity().expect(BREAKING_VALUE_KIND).x;
-            object.coordinate().y += object.velocity().expect(BREAKING_VALUE_KIND).y;
+            object.coordinate().x += object.velocity_mut().expect(BREAKING_VALUE_KIND).x;
+            object.coordinate().y += object.velocity_mut().expect(BREAKING_VALUE_KIND).y;
         }
     });
 
@@ -134,13 +164,21 @@ fn handle_connection(
 
     let get_circle = |o: &Box<dyn Object>| -> Circle {
         match (o.is_gravity_source(), o.is_gravity_receiver()) {
-            (true, false) => Circle { 
-                cx: o.get_coordinate().x, cy: o.get_coordinate().y, r: o.weight().expect(BREAKING_VALUE_KIND), stroke: "green".to_string(), 
-                fill: "black".to_string(), stroke_width: 3
+            (true, false) => Circle {
+                cx: o.coordinate().x,
+                cy: o.coordinate().y,
+                r: o.weight().expect(BREAKING_VALUE_KIND),
+                stroke: "green".to_string(),
+                fill: "black".to_string(),
+                stroke_width: 3,
             },
-            (false, true) => Circle { 
-                cx: o.get_coordinate().x, cy: o.get_coordinate().y, r: 2, stroke: "green".to_string(), 
-                fill: "black".to_string(), stroke_width: 3
+            (false, true) => Circle {
+                cx: o.coordinate().x,
+                cy: o.coordinate().y,
+                r: 2,
+                stroke: "green".to_string(),
+                fill: "black".to_string(),
+                stroke_width: 3,
             },
             (true, true) => todo!(),
             (false, false) => todo!(),
@@ -156,10 +194,14 @@ fn handle_connection(
     stream.flush().unwrap();
     stream.shutdown(std::net::Shutdown::Both).unwrap();
 
-    objects//.into_iter().map(|o| o.as_object()).collect::<Vec<Box<dyn Object>>>()
+    objects //.into_iter().map(|o| o.as_object()).collect::<Vec<Box<dyn Object>>>()
 }
 
-pub fn start_server(uri: &str, mut objects: Vec<Box<dyn Object>>, gravitational_constant: i32) -> ! {
+pub fn start_server(
+    uri: &str,
+    mut objects: Vec<Box<dyn Object>>,
+    gravitational_constant: i32,
+) -> ! {
     let listener = TcpListener::bind(uri).unwrap();
 
     for stream in listener.incoming() {
